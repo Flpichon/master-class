86abc120a2790e2106b8cdbe65c7d603a1ebb1b4
Franck <flepichon@b-p.fr>
2020-05-13 19:52:49 +0200

    fix bug

--- server/server.js
+++ server/server.js
@@ -10,10 +10,8 @@ app.start = function() {
     return app.listen(function() {
         app.emit('started');
         const baseUrl = app.get('url').replace(/\/$/, '');
-        console.log('Web server listening at: %s', baseUrl);
         if (app.get('loopback-component-explorer')) {
             const explorerPath = app.get('loopback-component-explorer').mountPath;
-            console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
         }
     });
 };
@@ -24,6 +22,6 @@ boot(app, __dirname, function(err) {
     if (err) throw err;
 
     // start the server if `$ node server.js`
-    if (require.main === module)
+    if (require.main === module || GLOBAL.PhusionPassenger)
         app.start();
 });
