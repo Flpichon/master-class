e77262cc556126959bbf51f27565b671cd351e02
Franck <flepichon@b-p.fr>
2020-05-14 10:31:55 +0200

    wip

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -60,6 +60,7 @@ export default {
       this.$store.dispatch('login', user)
         .then(() => this.$router.push('/'))
         .catch(err => {
+          console.log('err', err);
           this.$notify({
             type: 'error',
             group: 'foo',
