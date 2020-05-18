ece8a363718396c718d7850b01ba591cf3b7871f
Franck <flepichon@b-p.fr>
2020-05-18 14:55:51 +0200

    fix model

--- common/models/conference.js
+++ common/models/conference.js
@@ -5,7 +5,7 @@ module.exports = function(Conference) {
   Conference.beforeRemote('prototype.__create__articles', (ctx, instance, next) => {
     if (parseInt(ctx.args.data.authorId) 
       === parseInt(ctx.instance.userId) && parseInt(ctx.args.data.authorId) 
-      === parseInt(ctx.ctorArgs.options.accesstoken.userId)) {
+      === parseInt(ctx.ctorArgs.options.accessToken.userId)) {
         next();
       } else {
         const error = new Error('wrong user');
